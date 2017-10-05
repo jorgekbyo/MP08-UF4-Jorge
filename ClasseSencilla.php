62b1f1a1e53bff47ff48bbb7d64b813103b199f1
jorgekbyo <jorgegonzalezcabello@gmail.com>
2017-10-05 20:18:18 +0200

    Commit Inicial

---
+++ ClasseSencilla.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of newPHPClass
+ *
+ * @author Jorge
+ */
+
+      class ClaseSencilla
+{
+    // Declaración de una propiedad
+    public $var = 'un valor predeterminado';
+
+    // Declaración de un método
+    public function mostrarVar() {
+        echo $this->var;
+    }
+}
+
